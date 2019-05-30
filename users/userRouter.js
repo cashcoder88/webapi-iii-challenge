e027180cf11df2b49a819356c12c168ff390a5b5
cashcoder88 <cashglobe@gmail.com>
2019-05-30 07:26:11 -0600

    validatePost mw

--- users/userRouter.js
+++ users/userRouter.js
@@ -51,7 +51,10 @@ function validateUser(req, res, next) {
 };
 
 function validatePost(req, res, next) {
-
+    if (!req.body || !req.body.text) {
+        res.status(400).json({message: "missing required text field"})
+    }
+    next();
 };
 
 module.exports = router;
